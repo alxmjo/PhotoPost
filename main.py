7347511de2089f8e9fc16af97523653832435ec3
Alex Johnson <alexmontjohn@gmail.com>
2017-02-19 14:58:41 +0300

    Save

--- main.py
+++ main.py
@@ -1,1 +1,25 @@
-# main file
+import os
+import datetime
+
+# Get images from directory
+images = dict((i, '') for i in os.listdir())
+
+# Get title
+print('Title: ', end='')
+title = input()
+
+# Create filename and file
+now = datetime.datetime.now()
+print('Offset: ' + str(now.gmtime() - now.localtime()))
+file_name = str(now.year) + '-' + title.lower().replace(" ", "-") + '.markdown'
+file = open(file_name, "w+")
+
+for key in images.keys():
+    print('Image caption for ' + key + ': ', end='')
+    images[key] = input()
+
+# for key, value in images.items():
+
+
+
+print(images)
