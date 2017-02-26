49e0ecdbea95e2267407be5946090251e33024dc
Alex Johnson <alexmontjohn@gmail.com>
2017-02-26 08:31:02 +0300

    working draft without header creation

--- main.py
+++ main.py
@@ -1,25 +1,26 @@
 import os
-import datetime
+# import datetime
 
 # Get images from directory
 images = dict((i, '') for i in os.listdir())
 
-# Get title
-print('Title: ', end='')
-title = input()
+# # Get title
+# print('Title: ', end='')
+# title = input()
+#
+# # Create filename and file
+# now = datetime.datetime.now()
+# file_name = str(now.year) + '-' + str('%02d' % now.month) + '-' + str('%02d' % now.day) + '-' + \
+#             title.lower().replace(" ", "-") + '.markdown'
+# file = open(file_name, "w+")
 
-# Create filename and file
-now = datetime.datetime.now()
-print('Offset: ' + str(now.gmtime() - now.localtime()))
-file_name = str(now.year) + '-' + title.lower().replace(" ", "-") + '.markdown'
+file_name = 'temp.markdown'
 file = open(file_name, "w+")
 
 for key in images.keys():
     print('Image caption for ' + key + ': ', end='')
     images[key] = input()
 
-# for key, value in images.items():
-
-
-
-print(images)
+for key, value in images.items():
+    file.write('![' + value + '](/images/' + key + ')\n*' + value + '*')
+    file.write('\n\n\n\n')
